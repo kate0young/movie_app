a0bb2c3b1b0d756c9eaf37f529bffbc2d25bc390
animaluv <cf11hayato@gmail.com>
2019-04-09 16:51:34 +0900

    Updates

---
+++ precache-manifest.999b387489bf59c84cbb78a645bb6a0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd3ad5b808fe5930f9d2",
+    "url": "/movie_app/static/css/main.d5f0a88d.chunk.css"
+  },
+  {
+    "revision": "fd3ad5b808fe5930f9d2",
+    "url": "/movie_app/static/js/main.ad52a59e.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "73a8d673445c6a14ff235c1849e18f39",
+    "url": "/movie_app/index.html"
+  }
+];
